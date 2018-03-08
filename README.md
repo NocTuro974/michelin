d6d67ca48b0f75c3c7969cda5192d8018b671615
NocTuro974 <31305710+NocTuro974@users.noreply.github.com>
2018-03-09 00:56:23 +0100

    lafourchette.js operationnel, rendu final

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-Bonjour Monsieur, dans l'état actuel du programme (04/03), ce dernier est capable de
-scraper l'ensemble des établissements étoilés du Michelin.
-La partie Lafourchette est encore en développement car j'ai été malade ces derniers jours.
+Bonjour Monsieur, dans l'état actuel du projet (09/03), les deux scripts : server.js et lafourchette.js sont fonctionnels et permettent
+de récupérer toutes les promotions des restaurants étoilés sur lafourchette.
 
 Pour executer le script, il n'y a normalement rien à installer, car les packages sont dans le git.
-Commandes : "node server.js" et "node lafourchette.js" (ne fonctionne pas encore).
+Commandes : "node server.js" et "node lafourchette.js".
 
-Le server.js récupère dans un premier temps toutes les urls associées aux restaurants michelin étoilés, 
+Le server.js récupère dans un premier temps toutes les urls associées aux restaurants michelin étoilés,
 afin d'accéder à leur page individuelle et ce de manière asynchrone.
-Dans un second temps, toujours de façon asynchrone, le script va chercher le nom ainsi que le code postal du 
+Dans un second temps, toujours de façon asynchrone, le script va chercher le nom ainsi que le code postal du
 restaurant visé, afin de pouvoir comparer ces derniers avec la base de données de lafourchette.
 
 Les informations sont finalement enregistrées dans restos.json.
 Si vous tentez d'exécuter le script et qu'une erreur timeout survient, relancez directement l'intégralité du
 script, car les erreurs entrainent le rejet des promesses associées au requêtes et donc au promise.all.
+
+La fourchette.js se sert de l'api mobile et opère de la façon suivante :
+- récupère l'id du restaurant, s'il existe, parmi les résultats d'une requête par nom
+- récupère les promotions via une seconde requête, et les filtre pour ne donner que les résultats pertinents.
+- log les résultats directement dans la console.
+
+Bonne correction.
--- lafourchette.js
+++ lafourchette.js
@@ -4,31 +4,40 @@ var fs = require('fs');
 
 var listeRestos = [];
 var listeMichelin = [];
+var listePromesses = [];
+var listePromesses2 = [];
 
 
-function enregistrerResto(json){
+function enregistrerResto(json, restoRecherche){
   let i = 0;
   let trouve = false;
   let resto;
   while (i < json.length && trouve == false)
   for (i = 0; i < json.length; i++){
     resto = json[i];
-    if (resto.name == restoRecherche.name && resto.address.postal_code == restoRecherche.post){
+    restoRecherche.name = restoRecherche.name.toUpperCase();
+    if (restoRecherche.name.indexOf(resto.name.toUpperCase())>=0 && resto.address.postal_code == restoRecherche.post){
       trouve = true;
-      listeRestos.push(resto.id);
+      console.log(restoRecherche.name + " trouvé sur la fourchette");
+      listeRestos.push({"id" : resto.id, "name" : resto.name, "post" : resto.address.postal_code});
     }
   }
-  console.log(restoRecherche.name + " non trouve sur lafourchette");
+
+  if (trouve == false){
+    console.log(restoRecherche.name + ", " + restoRecherche.post + " pas trouvé sur lafourchette");
+  }
 }
 
-function creerPromesses(){
+function creerPromessesRecherche(listeMichelin){
   for (i = 0; i < listeMichelin.length; i++){
-    let url
+    restoRecherche=listeMichelin[i];
+    let url = "https://m.lafourchette.com/api/restaurant-prediction?name=" + encodeURIComponent(restoRecherche.name);
+    listePromesses.push(requestp(url, true, restoRecherche));
   }
 }
 
 
-function requestp(url, json) {
+function requestp(url, json, restoRecherche) {
     json = json || false;
     return new Promise(function (resolve, reject) {
         request({url:url, json:json}, function (err, res, body) {
@@ -41,22 +50,58 @@ function requestp(url, json) {
                 err.res = res;
                 return reject(err);
             }
-            console.log("requete terminée");
-            enregistrerResto(body);
+            enregistrerResto(body, restoRecherche);
             resolve(body);
         });
     });
 }
 
+function creerPromessesSales(){
+  return new Promise( function(resolve, reject){
+    console.log();
+    console.log("Recherche des promotions")
+    console.log();
+    for(i = 0; i < listeRestos.length; i++){
+      url = "https://m.lafourchette.com/api/restaurant/" + parseInt(listeRestos[i].id) + "/sale-type";
+      listePromesses2.push(requestSales(url, true, i));
+    }
+    console.log();
+    resolve();
+  });
+}
+
+function requestSales(url, json, index){
+  json = json || false;
+  return new Promise(function (resolve, reject) {
+    request({url:url, json:json}, function (err, res, body) {
+      if (err) {
+          console.log(err)
+          return reject(err);
+      } else if (res.statusCode !== 200) {
+          console.log(res)
+          err = new Error("Unexpected status code: " + res.statusCode);
+          err.res = res;
+          return reject(err);
+      }
+      if (body[0].hasOwnProperty("exclusions")){
+        if (body[0].title != "Simple booking"){
+          console.log("Le restaurant '" + listeRestos[index].name + "', dans le " + listeRestos[index].post + " dispose de promotions : ")
+          for (i = 0; i < body.length; i++){
+            if (body[i].hasOwnProperty("exclusions")){
+              console.log("- " + body[i].title)
+            }
+          }
+        console.log();
+        }
+      }
+    });
+  });
+}
 
 fs.readFile("restos.json", 'utf8', function(err, data) {
   if (err) throw err;
   listeMichelin = data;
   listeMichelin = JSON.parse(listeMichelin);
-  for(i = 0; i < 3; i++){
-    restoRecherche=listeMichelin[i];
-    let url = "https://m.lafourchette.com/api/restaurant-prediction?name=" + encodeURIComponent(restoRecherche.name);
-    console.log(restoRecherche.name)
-    requestp(url, true).then(enregistrerResto);
-  }
+  creerPromessesRecherche(listeMichelin);
+  Promise.all(listePromesses).then(creerPromessesSales)
 });
